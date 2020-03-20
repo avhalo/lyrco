94042bb30245f4024d97146403cbf7d3f349ed0f
Aa <avhalo@utu.fi>
2020-03-20 15:40:44 +0200

    sure you can think of million parts and tests

---
+++ main.py
@@ -0,0 +1,9 @@
+## This is the main function
+# For all the rest see the crawler.py
+# parser.py and possible rest
+# Those are however imported here too. 
+
+# For the function of this program you should see the 
+# README.md which contain info on such and the docs 
+# folder which has more on the structure on other
+# miscallenious information
